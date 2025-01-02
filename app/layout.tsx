34adc01389a1ce2bc8b6a72e782af6955ee61413
aya.mehry <aya.mehry30@gmail.com>
2025-01-02 20:39:23 +0100

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 export default function RootLayout({
   children,
 }: {
@@ -18,6 +19,7 @@ export default function RootLayout({
         <ThemeProvider enableSystem={true} attribute='class'>
           <Navbar />
           {children}
+          <Footer/>
         </ThemeProvider>
         </body>
     </html>
